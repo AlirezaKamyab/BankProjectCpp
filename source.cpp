b26bacd02e19515e06ca858ca941cc76bb4a83c6
Alireza Kamyab <alirezakamyab19@gmail.com>
2022-01-14 04:19:12 +0330

    tiny changes
    does not really matter now

--- source.cpp
+++ source.cpp
@@ -5,11 +5,38 @@
 #include "Headers/account.h"
 #include "Headers/facilities.h"
 #include "Headers/bank.h"
+#include "Headers/helperClass.h"
 using namespace std;
 
+void printMainMenu();
+void mainMenu();
+
+void clearConsole();
+void awaitKey();
 
 int main() {
-    Manager manager{"Alireza", "Kamyab", "1850484211", 102, Date{27, 4, 1381}, "aimlessly", "123456789"};
-    system("pause");
+    cout << Helper::getCurrentDate() << endl;
+    cout << (string)Helper::getCurrentTime() << endl;
+    awaitKey();
     return 0;
+}
+
+void printMainMenu() {
+    cout << "           Main Menu" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "1- Staff" << endl;
+    cout << "2- Client" << endl;
+    cout << "3- Exit" << endl;
+    cout << ">> ";
+}
+void mainMenu() {
+
+}
+
+void clearConsole() {
+    if(system("clear") == 0) return;
+    system("cls");
+}
+void awaitKey() {
+    system("pause");
 }
